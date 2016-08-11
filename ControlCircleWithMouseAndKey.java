60fdb45d1fc13d9f17f866c8d85b131f2e8ea723
Benjamin M6 <benjamynfrank101@gmail.com>
2016-08-11 20:53:47 +0100

    Completed the code for the ControlCircleWithMouseAndKey Which Basically creates a circle object and increases on mouse click or when the U key is pressed and decreases when the right mouse button is clicked or D key is press

--- ControlCircleWithMouseAndKey.java
+++ ControlCircleWithMouseAndKey.java
@@ -11,6 +11,49 @@ import javafx.stage.Stage;
 public class ControlCircleWithMouseAndKey extends Application{
 	private CirclePane circlePane = new CirclePane();
 
-	@Overide // Override the start method in the application class
-	
+	@Override // Override the start method in the application class
+	public void start (Stage primaryStage){
+		// Hold two buttons in an HBox
+		HBox hBox = new HBox();
+		hBox.setSpacing(10);
+		hBox.setAlignment(Pos.CENTER);
+		Button btEnlarge = new Button("Enlarge");
+		Button btShrink = new Button("Shrink");
+		hBox.getChildren().add(btEnlarge);
+		hBox.getChildren().add(btShrink);
+
+		// Create and register the handler
+		btEnlarge.setOnAction(e -> circlePane.enlarge());
+		btShrink.setOnAction(e -> circlePane.shrink());
+		circlePane.setOnMouseClicked(e -> {
+			if (e.getButton() == MouseButton.PRIMARY){
+				circlePane.enlarge();
+			}
+			else if (e.getButton() == MouseButton.SECONDARY){
+				circlePane.shrink();
+			}
+		});
+
+		circlePane.setOnKeyPressed(e -> {
+			if (e.getCode() == KeyCode.U){
+				circlePane.enlarge();
+			}
+			else if (e.getCode() == KeyCode.D){
+				circlePane.shrink();
+			}
+		});
+
+		BorderPane borderPane = new BorderPane();
+		borderPane.setCenter(circlePane);
+		borderPane.setBottom(hBox);
+		borderPane.setAlignment(hBox, Pos.CENTER);
+
+		// Create a scene and place it in the stage
+		Scene scene = new Scene(borderPane, 200,150);
+		primaryStage.setTitle("ControlCircle");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		circlePane.requestFocus();
+	}
 }
