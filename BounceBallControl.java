40071d5adede71fe11a7e603099d9625539b8f8e
Benjamin M6 <benjamynfrank101@gmail.com>
2016-08-19 21:00:57 +0100

    Listing 15.18 : added a java file that uses listing 15.17 to create a ball that bounces on the screen

---
+++ BounceBallControl.java
@@ -0,0 +1,37 @@
+/**Listing 15.18
+*BounceBallControl.java
+*/
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+
+public class BounceBallControl extends Application {
+  @Override // Override the start method in the Application class
+  public void start(Stage primaryStage){
+    BallPane ballPane = new BallPane(); // Create a ball Pane
+
+    // Pause and resume animation
+    ballPane.setOnMousePressed(e -> ballPane.pause());
+    ballPane.setOnMouseReleased(e -> ballPane.play());
+
+    // Increase and decrease animation
+    ballPane.setOnKeyPressed(e -> {
+      if (e.getCode() == KeyCode.UP){
+        ballPane.increaseSpeed();
+      }
+      else if (e.getCode() == KeyCode.DOWN){
+        ballPane.decreaseSpeed();
+      }
+    });
+
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(ballPane, 250, 150);
+    primaryStage.setTitle("BounceBallControl");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+
+    // Must request focus after the primary stage is displayed
+    ballPane.requestFocus();
+  }
+}
